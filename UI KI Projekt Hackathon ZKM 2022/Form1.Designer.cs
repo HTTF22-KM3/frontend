0592fdd8691dde24030f1d36f11096b76a864a6c
Martin-Moras <martin.moras@web.de>
2022-05-08 17:02:05 +0200

    added zooooooooooom

--- UI KI Projekt Hackathon ZKM 2022/Form1.Designer.cs
+++ UI KI Projekt Hackathon ZKM 2022/Form1.Designer.cs
@@ -32,16 +32,20 @@
             this.button1 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ZoomBar = new System.Windows.Forms.TrackBar();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ZoomBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(603, 246);
+            this.button1.Location = new System.Drawing.Point(712, 391);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(237, 113);
+            this.button1.Size = new System.Drawing.Size(178, 37);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -49,28 +53,55 @@
             // 
             // panel1
             // 
+            this.panel1.AllowDrop = true;
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.AutoScroll = true;
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Location = new System.Drawing.Point(4, 1);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(872, 245);
+            this.panel1.Size = new System.Drawing.Size(872, 384);
             this.panel1.TabIndex = 2;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(266, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(0, -3);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(1280, 720);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // ZoomBar
+            // 
+            this.ZoomBar.AllowDrop = true;
+            this.ZoomBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ZoomBar.Location = new System.Drawing.Point(160, 391);
+            this.ZoomBar.Maximum = 100;
+            this.ZoomBar.Name = "ZoomBar";
+            this.ZoomBar.Size = new System.Drawing.Size(546, 56);
+            this.ZoomBar.TabIndex = 3;
+            this.ZoomBar.Value = 5;
+            this.ZoomBar.Scroll += new System.EventHandler(this.ZoomBar_Scroll);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(4, 401);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(150, 27);
+            this.numericUpDown1.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(843, 371);
+            this.ClientSize = new System.Drawing.Size(893, 440);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.ZoomBar);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -78,7 +109,10 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ZoomBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -86,5 +120,7 @@
         private Button button1;
         private Panel panel1;
         private PictureBox pictureBox1;
+        private TrackBar ZoomBar;
+        private NumericUpDown numericUpDown1;
     }
 }
--- UI KI Projekt Hackathon ZKM 2022/Form1.cs
+++ UI KI Projekt Hackathon ZKM 2022/Form1.cs
@@ -6,15 +6,47 @@ namespace UI_KI_Projekt_Hackathon_ZKM_2022
 {
     public partial class Form1 : Form
     {
+        private Image img;
 
         public Form1()
         {
             InitializeComponent();
+            setZoomSlider();
+
+            this.img = Image.FromFile(@"C:\Users\Notebook-ADM\Downloads\image.jpg");
+            pictureBox1.Image = this.img;
+            ZoomBar_Scroll(new object(), new EventArgs());
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            ProcessStartInfo start = new ProcessStartInfo();
+
+        }
+
+        private void ZoomBar_Scroll(object sender, EventArgs e)
+        {
+            pictureBox1.Image = ZoomPicture(this.img);
+        }
+        private void setZoomSlider()
+        {
+            ZoomBar.UseWaitCursor = false;
+            this.DoubleBuffered = true;
+        }
+        Bitmap ZoomPicture(Image img)
+        {
+            float zoomValue = (Convert.ToSingle(ZoomBar.Value) + 1.0f) / 100.0f;
+
+            return new Bitmap(img, Convert.ToInt32(img.Width * zoomValue), Convert.ToInt32(img.Height * zoomValue));
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+/*ProcessStartInfo start = new ProcessStartInfo();
             start.FileName = @"C:\Users\kacpe\AppData\Local\Programs\Python\Python38-32\python.exe";
             start.Arguments = @"main.py";
             start.WorkingDirectory = @"C:\Users\kacpe\PycharmProjects\Palindrom";
@@ -28,18 +60,4 @@ namespace UI_KI_Projekt_Hackathon_ZKM_2022
                     MessageBox.Show(result);
 
                 }
-            }
-
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-    }
-}
+            }*/
